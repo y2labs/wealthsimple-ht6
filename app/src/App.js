155da3959c0438f5610319ee6f5aea8be9fe60f0
danielkooeun <eun.koo@mail.utoronto.ca>
2018-06-18 00:19:34 -0400

    setup notification - need to add location, info, etc.

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,20 @@ import AuthenticationProvider from 'AuthenticationProvider';
 import Routes from 'Routes';
 
 class App extends Component {
+  componentDidMount() {
+    if (Notification.permission === "granted") this.notify();
+  }
+
+  notify() {
+    // some sort of method to poll for new notifications?
+    var notification = new Notification('TITLE', {
+      // icon: '',
+      body: 'TEST',
+    });
+
+    setTimeout(notification.close.bind(notification), 5000);
+  }
+
   render() {
     return (
       <AuthenticationProvider>
